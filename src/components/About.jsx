f3d85ce938ab1170229672955a678504fa2f5ff4
jywkim <jkim589@gmail.com>
2022-07-05 23:34:28 -0400

    install react-router-dom and implement routing

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">About</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis unde maxime dolore consectetur nisi facilis, aliquam modi ducimus amet aspernatur praesentium itaque quae officia rerum autem consequatur quibusdam odio ullam?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
